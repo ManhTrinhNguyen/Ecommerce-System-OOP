8e0ac561fd7958ee4e94dc4c31dc14d358df2d76
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-08-31 17:55:26 -0700

    Created Product class with product_id, name, price, quantity and 2 method Reduce_stock and incease_stock

---
+++ product.py
@@ -0,0 +1,24 @@
+class Product:
+   # Instances attribute 
+  def __init__(self, product_id: int, name: str, price: int, quantity: int) -> None:
+    self.product_id = product_id
+    self.name = name 
+    self.price = price 
+    self.quantity = quantity
+
+  # Medthod 
+  def __str__(self) -> str:
+    return f"Product name: {self.name}, Product_Id: {self.product_id}, Price: {self.price}, Quantity: {self.quantity}"
+  
+  # Reduce Stock funtion 
+  def reduce_stock(self, quantity):
+    if self.quantity >= quantity: # Check if Quantity of available stock is greater the given quantity want to reduce 
+      self.quantity = self.quantity - quantity # If True => Reduce given quantity want to reduce 
+      return f"Reduced {quantity} amount of stocks"
+    else:
+      return f"Not enough stock to reduce available stock is {self.quantity}" # If false return Not Enough stock 
+    
+  # Increase Stock funtion 
+  def increase_stock(self, quantity):
+    self.quantity = self.quantity + quantity # Add stock by given quantity to original quantity of the product 
+    return f"Added {quantity} amount of stock"
